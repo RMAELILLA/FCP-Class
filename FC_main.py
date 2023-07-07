e7f5cdd9d6e19bdef226a45a71d7937eb419522d
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 21:54:59 +0800

    get two object from user input to assign

--- FC_main.py
+++ FC_main.py
@@ -3,7 +3,14 @@ from FCuserdisplay import FCUSerDisplay
 
 class TestFan:
     def __init__(self):
-        self.first_fan = self.assign_fan("First Fan: ")
-        self.second_fan = self.assign_fan("Second Fan: ")
+        self.first_fan = self.FCUD.assign_fan("First Fan: ")
+        self.second_fan = self.FCUD.assign_fan("Second Fan: ")
         self.Fan = Fan()
         self.FCUD = FCUSerDisplay()
+    def fan_properties(self):
+        print("First Fan: ")
+        self.fan_properties(self.first_fan)
+
+        print("\nSecond Fan: ")
+        self.fan_properties(self.second_fan)
+        
