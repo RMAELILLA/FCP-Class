0effdf6bbad32a3863521f6056f8d1a3a1255462
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 19:37:18 +0800

    set get ON

--- FClass.py
+++ FClass.py
@@ -11,11 +11,16 @@ class Fan:
         self.__radius = radius
         self.__color = color
 
-        
-# A private data field named speed that specifies whether the fan is on (the default is False)
-# A private float data field named radius that specifies the radius of the fan
-# A private string data field named color that specifies the color of the fan
+#checked    # A private data field named speed that specifies whether the fan is on (the default is False)
+            # A private float data field named radius that specifies the radius of the fan
+            # A private string data field named color that specifies the color of the fan
+
 # The accessor (getters) and mutator(setters) methods for all four data fields.
+    def fanOn(self):
+        return self.__on
+    def setOn(self, on):
+        self.on = on
+
 # A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False)
 
 # Write a test program named TestFan that creates two fan objects. For the first object, assign a maximum speed, radius 10, color yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object's speed, radius, color, and on properties.
