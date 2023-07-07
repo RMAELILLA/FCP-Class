c9cb1fbee05045ae7c4854d5dff3590fa5944be7
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 22:26:18 +0800

    cleaning to locate errors

--- FClass.py
+++ FClass.py
@@ -1,6 +1,4 @@
-# Design a class named Fan to represent a fan. The class contains:
 class Fan:
-# Three constant named SLOW, MEDIUM, and FAST, with the values 1,2, and 3 to denote the fan speed
     SLOW = 1
     MEDIUM = 2
     FAST = 3
@@ -10,12 +8,7 @@ class Fan:
         self.__speed = speed
         self.__radius = radius
         self.__color = color
-
-#checked    # A private data field named speed that specifies whether the fan is on (the default is False)
-            # A private float data field named radius that specifies the radius of the fan
-            # A private string data field named color that specifies the color of the fan
-
-# The accessor (getters) and mutator(setters) methods for all four data fields.
+ 
     def fanOn(self):
         return self.__on
     def setOn(self, on):
@@ -32,4 +25,3 @@ class Fan:
         return self.__color
     def setColor(self, color):
         self.__color = color
-# A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False)
