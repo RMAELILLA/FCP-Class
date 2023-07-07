b740371911c1e788e9e4fc4aa4235988615f1cd5
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 21:32:56 +0800

    separate variables

--- FC_main.py
+++ FC_main.py
@@ -1,24 +1,9 @@
 from FClass import Fan
+from FCuserdisplay import FCUSerDisplay
+
 class TestFan:
     def __init__(self):
         self.first_fan = self.assign_fan("First Fan: ")
         self.second_fan = self.assign_fan("Second Fan: ")
-        self.Fan_display()
-    
-    def assign_fan(self):
-        print("Assign Fan object: ")
-        input(\
-        """
-        What properties do you want in your fan?
-        
-        SPEED:
-        1 = SLOW
-        2 = MEDIUM
-        3 = FAST
-
-        RADIUS:
-        1-10
-
-        COLOR (can be anything you want):
-        """)
-        
+        self.Fan = Fan()
+        self.FCUD = FCUSerDisplay()
--- FCuserdisplay.py
+++ FCuserdisplay.py
@@ -1,18 +1,23 @@
 class FCUSerDisplay:
     def assign_fan(self):
         print("Assign Fan object: ")
-        input(\
+        speed = (input(\
         """
-        What properties do you want in your fan?
+        What speed properties do you want in your fan?
         
         SPEED:
         1 = SLOW
         2 = MEDIUM
         3 = FAST
-
+        """))
+        
+        radius = input(\
+        """
         RADIUS:
         1-10
+        """)
 
+        color = input(\
+        """
         COLOR (can be anything you want):
         """)
-        
