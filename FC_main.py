918ee3486aeb74d865b88ab1f542c7bbe91e15c7
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 22:29:47 +0800

    successful code overlook naming functions

--- FC_main.py
+++ FC_main.py
@@ -6,12 +6,14 @@ class TestFan:
         self.FCUD = FCUSerDisplay()
         self.first_fan = self.FCUD.assign_fan("First Fan: ")
         self.second_fan = self.FCUD.assign_fan("Second Fan: ")
+
     def fan_properties(self):
         print("First Fan: ")
-        self.fan_properties(self.first_fan)
+        self.fan_object(self.first_fan)
 
         print("\nSecond Fan: ")
-        self.fan_properties(self.second_fan)
+        self.fan_object(self.second_fan)
+
     def fan_object(self, fan):
         print("ON: ", fan.fanOn())
         print("Speed: ", fan.getSpeed())
