9d79b9ecdc7921ecde69dd9df2b89fb7ee793e69
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-07 21:19:59 +0800

    let user assign fan objects

--- FC_main.py
+++ FC_main.py
@@ -1,18 +1,24 @@
 from FClass import Fan
-
-# Write a test program named TestFan that creates two fan objects. For the first object, assign a maximum speed, radius 10, color yellow, and turn it on.
 class TestFan:
     def __init__(self):
-        self.Fan = Fan()
-    input(\
-    """
-    What do you want to change?
-    1 = speed
-    2 = radius
-    3 = color
-
-    Your choice: 
-    """)
+        self.first_fan = self.assign_fan("First Fan: ")
+        self.second_fan = self.assign_fan("Second Fan: ")
+        self.Fan_display()
     
-# Assign medium speed, radius 5, color blue, and turn it off for the second object.
-# Display each object's speed, radius, color, and on properties.
+    def assign_fan(self):
+        print("Assign Fan object: ")
+        input(\
+        """
+        What properties do you want in your fan?
+        
+        SPEED:
+        1 = SLOW
+        2 = MEDIUM
+        3 = FAST
+
+        RADIUS:
+        1-10
+
+        COLOR (can be anything you want):
+        """)
+        
