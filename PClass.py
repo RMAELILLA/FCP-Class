20c0cea7bb25410db0f1c3f9d326fa22afa3e93d
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 20:33:41 +0800

    planning program

---
+++ PClass.py
@@ -0,0 +1,16 @@
+# Write a class named Pet, which should have the following data attributes:
+# __name (for the name of the pet)
+# __animan_type (for the type of animal that a pet is. Example values are 'Dog', 'Cat', and 'Bird')
+# __age (for the pet's age)
+
+# The pet class should have an __init__ method that creates these attributes. It should also have the following methods:
+# setName() # This method assigns a value to the __name field.
+# setAnimal_type() # This method assigns a value to the __animal_type field.
+# setAge # This method assigns a value to the __age field.
+# getName() # This method returns the value of the __name field.
+# getAnimal_type() # This method returns the value of the __animal_type field.
+# getAge() # This method returns the value of the __age field.
+
+# Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, age of his or her pet.
+# This data should be stored as the object's attributes.
+# Use the object's accessor methods to retrieve the pet's name, type, and age and display this data on the screen.
