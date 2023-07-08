0e33e9dca100b243eba25d30996457146835de3b
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 21:50:47 +0800

    function variable

--- PC_main.py
+++ PC_main.py
@@ -2,6 +2,17 @@ from tkinter import Tk, Label, Entry, Button
 from PClass import Pet
 from PCuserdisplay import userdisplay, userPetinfo
 
+def GUI():
+    root = Tk()
+    root.title("Pet Information")
+
+    name_label = Label(root, text="Name: ")
+    name_entry = Entry(root)
+    Animal_type_label = Label(root, text="Animal Type: ")
+    Animal_type_entry = Entry(root)
+    age_level = Label(root, text="Age: ")
+    age_entry = Entry(root)
+
 def main():
     pet = Pet()
 
