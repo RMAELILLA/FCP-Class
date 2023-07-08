e2e7f01adf11c61a79ae0be0ff0827c2e741c14b
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 14:56:51 +0800

    pseudocode planning

---
+++ Cclass.py
@@ -0,0 +1,18 @@
+# Write a class named car that has the following data attributes:
+# __year_model (for the car's year model)
+# __make (for the make of the car)
+# __speed (for the car's current speed)
+
+# The car class should have an __init__ method that accepts the car's year model and make as arguments.
+# These values should be assigned to the object's __year_model and __make data attributes.
+# It should also assign 0 to the __speed data attribute.
+
+# The class should also have the following methods:
+# Accelerate() # accelerate method should add 5 from the speed data attribute each time it is called
+# Break() # break method should subtract 5 from the speed data attribute each time it is called.
+# getSpeed() # The getSpeed method should return the current speed
+
+# Next, desing a program that creates a Car object then calls the accelerate method five times.
+# After each call to the accelerate method, get the current speed of the car and display it.
+# Then call the brake method five times.
+# After each call to the break method, get the current speed of the car and display it.
