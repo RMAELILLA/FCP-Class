9972ccdc8ac85ea3a0fe1a125c96fc7b3489f0b8
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 21:22:18 +0800

    change return and call functions

--- PC_main.py
+++ PC_main.py
@@ -1,3 +1,2 @@
-# Write a program that creates an object of the class and prompts the user to enter the name, type, age of his or her pet.
-# This data should be stored as the object's attributes.
-# Use the object's accessor methods to retrieve the pet's name, type, and age and display this data on the screen.
+from PClass import Pet
+from PCuserdisplay import userdisplay, userPetinfo
