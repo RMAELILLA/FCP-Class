7bb834c1c162e6d37aa1462fffc99453fc51d81c
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 16:28:21 +0800

    incorrect call and files

--- CC_main.py
+++ CC_main.py
@@ -6,3 +6,7 @@ class Cmain:
         make = input("Enter the make of the car: ")
     
         car = Car(year_model, make)
+
+        for _ in range (5):
+            car.accelerate()
+            print("Current Speed: ", car.get_speed())
