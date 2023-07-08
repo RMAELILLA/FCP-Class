682995db9f5f88fddabe1d5514258be84d79d2c1
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-08 21:24:49 +0800

    no errors continue code

--- PC_main.py
+++ PC_main.py
@@ -1,2 +1,11 @@
 from PClass import Pet
 from PCuserdisplay import userdisplay, userPetinfo
+
+def PCUD():
+    pet = Pet()
+
+    name, animal_type, age = userdisplay()
+
+    pet.setName(name)
+    pet.setAnimal_type(animal_type)
+    pet.setAge(age)
